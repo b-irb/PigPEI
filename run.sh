7d1acc18493a4f6473921e86e0b1c5f0e43efb9d
birb007 <25516647+birb007@users.noreply.github.com>
2023-05-19 23:47:41 +0100

    new debug script

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ TARGET=target/x86_64-none-uefi/debug/pig.efi
 cargo build
 
 [ -f $TARGET ] || exit 1
-toolchain/injector.py $TARGET OVMF_CODE.fd $FV/OVMF_CODE.fd
+uefireplace $FV/OVMF_CODE.fd $(cat uuid) 10 $TARGET 1>/dev/null
 
 # Create a UEFI environment with mounted OVMF firmware and
 # ISA exit device mapped at 0x501 I/O address.
@@ -16,7 +16,8 @@ qemu-system-x86_64 \
     -s \
     -nographic \
     -machine type=q35,accel=kvm:tcg \
-    -drive file=$FV/OVMF_CODE.fd,format=raw,if=pflash \
+    -m 512 -hda alpine.img -boot c \
+    -drive file=$FV/OVMF_CODE.fd.patched,format=raw,if=pflash \
     -drive file=$FV/OVMF_VARS.fd,format=raw,if=pflash \
     -device isa-debug-exit \
     -debugcon file:debug.log -global isa-debugcon.iobase=0x402 \
